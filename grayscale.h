6ddcb5eede11a2e42fe82f2900df806b6761a0aa
Thomas CORBIERE <76699290+realTartiflette@users.noreply.github.com>
2021-10-06 13:12:05 +0200

    Delete grayscale.h

--- grayscale.h
+++
@@ -1,9 +0,0 @@
-#ifndef GRAYSCALE_H
-#define GRAYSCALE_H
-
-
-Uint32 GetPixel(SDL_Surface* surface, int x, int y);
-void PutPixel(SDL_Surface* surface, int x, int y, Uint32 pixel);
-char* Grayscale(char path[]);
-
-#endif
