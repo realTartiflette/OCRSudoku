5bf17d94fd66f781908756a19040417d40fc3372
olivier.talane <olivier.talane@epita.fr>
2021-10-24 15:27:02 +0200

    sans les commentaires

--- solver/solver.c
+++ solver/solver.c
@@ -27,13 +27,10 @@ int number_unassigned(int *row, int *col)
     {
         for(j=0;j<9;j++)
         {
-            //cell is unassigned
             if(matrix[i][j] == 0)
             {
-                //changing the values of row and col
                 *row = i;
                 *col = j;
-                //there is one or more unassigned cells
                 num_unassign = 1;
                 return num_unassign;
             }
@@ -45,21 +42,16 @@ int number_unassigned(int *row, int *col)
 int is_safe(int n, int r,int c)
 {
     int i,j;
-    //checking in row
     for(i=0;i<9;i++)
     {
-        //there is a cell with same value
         if(matrix[r][i] == n)
             return 0;
     }
-    //checking column
     for(i=0;i<9;i++)
     {
-        //there is a cell with the value equal to i
         if(matrix[i][c] == n)
             return 0;
     }
-    //checking sub matrix
     int row_start = (r/3)*3;
     int col_start = (c/3)*3;
     for(i=row_start;i<row_start+3;i++)
@@ -79,24 +71,16 @@ int solve_sudoku()
 {
     int row;
     int col;
-    //if all cells are assigned then the sudoku is already solved
-    //pass by reference because number_unassigned will change the values of row and col
     if(number_unassigned(&row, &col) == 0)
         return 1;
     int i;
-    //number between 1 to 9
     for(i=1;i<=9;i++)
     {
-        //if we can assign i to the cell or not
-        //the cell is matrix[row][col]
         if(is_safe(i, row, col))
         {
             matrix[row][col] = i;
-            //backtracking
             if(solve_sudoku())
                 return 1;
-            //if we can't proceed with this solution
-            //reassign the cell
             matrix[row][col]=0;
         }
     }
@@ -119,10 +103,9 @@ int main(int argc, char* argv[])
         {
             int i=0;
             int j=0;
-            // Boucle de lecture des caractères un à un
             do
             {
-                caractereActuel = fgetc(fichier); // On lit le caractère
+                caractereActuel = fgetc(fichier); 
                 if (j==9)
                 {
                     j=0;
@@ -139,11 +122,10 @@ int main(int argc, char* argv[])
                     matrix[i][j]=(int)caractereActuel-48;
                     j=j+1;
                 }
-                //printf("%c", caractereActuel); // On l'affiche
 
 
 
-            } while (caractereActuel != EOF && i!=9); // On continue tant que fgetc n'a pas retourné EOF (fin de fichier)
+            } while (caractereActuel != EOF && i!=9); 
     
             fclose(fichier);
         }
